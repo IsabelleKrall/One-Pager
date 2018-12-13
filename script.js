5f0ede5c03e776c6a17c9936cbf4d62615f5323b
Adrian Jungnelius <a.jungnelius@gmail.com>
2018-12-13 14:40:48 +0100

    final commit

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-'use strict'
-const icon = document.querySelector('.icon');
-const modal = document.querySelector('.modal');
-const textitem = document.querySelectorAll('.text-item');
+"use strict";
+const icon = document.querySelector(".icon");
+const modal = document.querySelector(".modal");
+const textitem = document.querySelectorAll(".text-item");
 
-icon.addEventListener('click', () => {
-  icon.classList.toggle('active');
-  modal.classList.toggle('active');
+icon.addEventListener("click", () => {
+  icon.classList.toggle("active");
+  modal.classList.toggle("active");
   // body.classList.toggle('menuopen');
 });
 
-Array.from(textitem).forEach((text) => {
-  text.classList.toggle('active');
-  text.addEventListener('click', () => {
-    modal.classList.toggle('active');
-    icon.classList.toggle('active');
-    body.classList.toggle('menuopen');
+Array.from(textitem).forEach(text => {
+  text.classList.toggle("active");
+  text.addEventListener("click", () => {
+    modal.classList.toggle("active");
+    icon.classList.toggle("active");
+    body.classList.toggle("menuopen");
   });
 });
 
 //Move the arrow up on mouseover:
-const arrowUp = document.querySelector('.to-top')
-const arrow = document.querySelector('.arrow')
-const newArrow = document.querySelector('.to-top img')
+const arrowUp = document.querySelector(".to-top");
+const arrow = document.querySelector(".arrow");
+const newArrow = document.querySelector(".to-top img");
 
-
-arrowUp.addEventListener('mouseover', () => {
-  arrow.classList.add('move');
-  newArrow.setAttribute("src", "img/pil-upp-bla.svg")
+arrowUp.addEventListener("mouseover", () => {
+  arrow.classList.add("move");
+  newArrow.setAttribute("src", "img/pil-upp-bla.svg");
 });
 
-arrowUp.addEventListener('mouseout', () => {
+arrowUp.addEventListener("mouseout", () => {
   newArrow.setAttribute("src", "img/pil-upp.svg");
 });
